3eea4406f87f2aec039cc8ec9e7f91c7e81ad98e
johnjainc <116500864+johnjainc@users.noreply.github.com>
2022-12-02 19:52:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var http = require("http");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello \n');
-}).listen(3000);
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
